14c3a2dd1c99762f4a82dd01b79f27cf2c4e5ca8
s0044613 <sama.mushtaq@rgare.com>
2018-11-06 12:49:34 -0600

    Update chapter4_Data_Structures_Objects_and_Arrays/List.js

--- chapter4_Data_Structures_Objects_and_Arrays/List.js
+++ chapter4_Data_Structures_Objects_and_Arrays/List.js
@@ -1,3 +1,14 @@
+/*
+Write a function arrayToList that builds up a list structure like the one
+shown when given [1, 2, 3] as argument. Also write a listToArray function
+that produces an array from a list. Then add a helper function prepend, which
+takes an element and a list and creates a new list that adds the element to the
+front of the input list, and nth, which takes a list and a number and returns
+the element at the given position in the list (with zero referring to the first
+element) or undefined when there is no such element.
+If you haven’t already, also write a recursive version
+*/
+
 //This function creates an array of objects and sets the value property to the corresponding item of the given array
 function arrayToList(arrayToCovert) {
   let listArray = [];
@@ -55,7 +66,7 @@ console.log(newArray);
 
 /*function which
 takes an element and a list and creates a new list that adds the element to the
-front of the input list*/
+front of the given list*/
 function prepend(item, list) {
 
   let listItem = {};
